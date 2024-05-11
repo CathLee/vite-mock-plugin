7a36dac066d7bff76b401d0dfa691debe5fbbb4e
CathLee <447932704@qq.com>
2024-05-11 22:51:16 +0800

    chore:init

---
+++ .prettier.js
@@ -0,0 +1,32 @@
+/*
+ * @Date: 2024-05-11 22:44:38
+ * @Description: 
+ */
+module.exports = {
+    printWidth: 100,
+    tabWidth: 2,
+    useTabs: false,
+    semi: true,
+    vueIndentScriptAndStyle: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    trailingComma: 'es5',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    arrowParens: 'always',
+    insertPragma: false,
+    requirePragma: false,
+    proseWrap: 'never',
+    htmlWhitespaceSensitivity: 'strict',
+    endOfLine: 'lf',
+    rangeStart: 0,
+    overrides: [
+      {
+        files: '*.md',
+        options: {
+          tabWidth: 2,
+        },
+      },
+    ],
+  };

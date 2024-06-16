f9c3403edc065ee1e78b1f45cb66b3a6e856b32d
CathLee <447932704@qq.com>
2024-06-16 14:11:55 +0800

    chore:eslint init

--- playground/js-example/vite.config.js
+++ playground/js-example/vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
+/*
+ * @Date: 2024-06-16 11:18:58
+ * @Description:
+ */
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
+import {ViteMockServer} from "../../src/index.js";
+import process from "process";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+    plugins: [
+        vue(),
+        ViteMockServer({
+            ignore: /^_/,
+            mockPath: "mock",
+            watchFiles: true,
+            localEnabled: process.env.NODE_ENV === "development",
+        }),
+    ],
+});
